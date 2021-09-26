fc6ffeec2e3599b365974106af8ab882b4b64cb9
unknown <danielbrunno1@gmail.com>
2021-09-26 20:23:43 -0300

    Subindo Menu Lateral

--- view/menuAdmin.php
+++ view/menuAdmin.php
@@ -487,7 +487,7 @@ body {
 
 <div class="page-wrapper chiller-theme toggled">
   <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
-    <i class="fas fa-bars"></i>
+    
   </a>
   <nav id="sidebar" class="sidebar-wrapper">
     <div class="sidebar-content">
